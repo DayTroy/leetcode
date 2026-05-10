12e52007e3f62d7907f0489a4a1bfffcbf024d62
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-05-10 12:45:17 +0300

    Add: permutation-string - submission-13

---
+++ Data Structures & Algorithms/permutation-string/submission-13.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    // s1 = "abc", s2 = "lecabee"
+
+    checkInclusion(s1, s2) {
+        const freq1 = new Map();
+
+        for (let char of s1) {
+            freq1.set(char, (freq1.get(char) || 0) + 1)
+        }
+
+        const unique = freq1.size;
+
+        for (let i = 0; i < s2.length; i++) {
+            let count = 0;
+            const freq2 = new Map();
+
+            for (let j = i; j < s2.length; j++) {
+                const char = s2[j];
+                if (!freq1.has(char)) break;
+
+                freq2.set(char, (freq2.get(char) || 0) + 1);
+
+                if (freq2.get(char) > freq1.get(char)) break;
+
+                if (freq1.get(char) === freq2.get(char)) {
+                    count++;
+                }
+
+                if (count === unique) return true;
+            }
+        }
+
+        return false;
+    }
+}
