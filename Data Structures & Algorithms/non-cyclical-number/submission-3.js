49deb46845ea798a68a706097e960246c4542da9
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-05-10 23:03:47 +0300

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    isHappy(n) {
+        const sums = [];
+
+        let nStr = n.toString();
+        let currentSum = 0; 
+
+        while (true) {
+            if (currentSum === 1) return true;
+
+            currentSum = nStr
+                .split('')
+                .map(item => parseInt(item))
+                .reduce((acc, prev) => acc + prev ** 2, 0);
+
+            if (sums.includes(currentSum)) return false;
+            sums.push(currentSum);
+            nStr = currentSum.toString();
+        }
+    }
+}
