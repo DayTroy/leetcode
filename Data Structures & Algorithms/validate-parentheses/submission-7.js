7ccef1f851f85933c69437b4fb9e46591c4a2f2e
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-05-12 21:31:55 +0300

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const check = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+        }
+
+        for (let skobka of s) {
+            if (skobka === '(' || skobka === '{' || skobka == '[') {
+                stack.push(skobka);
+            } else if (skobka !== check[stack.pop()]) return false;
+        }
+
+        return stack.length === 0;
+    }
+}
