911d9bec502ca7342801e7ae98222fc319dc1b72
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-06-07 23:25:02 +0300

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.js
@@ -0,0 +1,47 @@
+// class Node {
+//     constructor(key, value) {
+
+//     }
+// }
+
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.cache = new Map();
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        if (!this.cache.has(key)) return -1;
+
+        const value = this.cache.get(key);
+
+        this.cache.delete(key);
+        this.cache.set(key, value);
+
+        return value;
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        if (this.cache.get(key)) {
+            this.cache.delete(key);
+        }
+
+        this.cache.set(key, value);
+
+        if (this.capacity < this.cache.size) {
+            this.cache.delete(this.cache.keys().next().value);
+        }
+    }
+}
