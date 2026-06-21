719d1eec4e7a8c043baf46dd5b3e1a862b62ec82
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-06-21 20:43:48 +0300

    Add: balanced-binary-tree - submission-11

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-11.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+
+    isBalanced(root) {
+        return this.checkHeight(root) !== -1;
+    }
+
+    checkHeight(node) {
+        if (!node) return 0;
+
+        const left = this.checkHeight(node.left);
+        if (left === -1) return -1;
+
+        const right = this.checkHeight(node.right);
+        if (right === -1) return -1;
+
+        if (Math.abs(left - right) > 1) return -1;
+
+        return 1 + Math.max(left, right);
+    }
+}
