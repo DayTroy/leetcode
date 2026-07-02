3599fc4c0813a7cdfcb2c8f59d41f535585a6e4d
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-07-02 20:47:52 +0300

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+
+    // ["act","pots","tops","cat","stop","hat"]
+
+    groupAnagrams(strs) {
+        const data = new Map();
+
+        for (let str of strs){
+            const key = [...str].toSorted().join('');
+            if (data.has(key)) {
+                data.set(key, [...data.get(key), str])
+            } else {
+                data.set(key, [str]);
+            }
+        }
+
+
+
+        return [...data.values()];
+    }
+}
