c0528e6bbe1fd3966053e0cc54dd7c3b034825ec
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-07-03 20:34:41 +0300

    Add: longest-repeating-substring-with-replacement - submission-19

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-19.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    //  "XYYX"
+    // X: 1
+    // Y: 1
+
+    characterReplacement(s, k) {
+        let longest = 0;
+
+        for (let i = 0; i < s.length; i++) {
+            const frequency = new Map();
+            let maxF = 0;
+            for (let j = i; j < s.length; j++) {
+                frequency.set(s[j], (frequency.get(s[j]) || 0) + 1);
+                maxF = Math.max(maxF, frequency.get(s[j]));
+
+                if (j - i + 1 - maxF  > k) break;
+                else {
+                    longest = Math.max(longest, j - i + 1);
+                }
+            }
+        }
+
+        return longest;
+    }
+}
