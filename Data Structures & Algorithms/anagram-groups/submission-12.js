393956b425a26396262ddd1b7dee80586898a865
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-07-07 00:20:40 +0300

    Add: anagram-groups - submission-12

---
+++ Data Structures & Algorithms/anagram-groups/submission-12.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    // ["act","pots","tops","cat","stop","hat"]
+
+    groupAnagrams(strs) {
+        const res = new Map();
+        for (let str of strs) {
+            let count = new Array(26).fill(0);
+
+            for (let char of str) {
+                count[char.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+            }
+
+            const key = count.join(',')
+
+            if (res.has(key)) {
+                res.set(key, [...res.get(key), str])
+            } else {
+                res.set(key, [str])
+            }
+        }
+
+        return [...res.values()];
+    }
+}
