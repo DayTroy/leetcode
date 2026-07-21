b5c85780567ba22ed34339bfbd951020420803a3
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-07-21 20:28:08 +0300

    Add: car-fleet - submission-21

---
+++ Data Structures & Algorithms/car-fleet/submission-21.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const cars = position
+            .map((pos, index) => [pos, speed[index]])
+            .sort((a, b) => b[0] - a[0]);
+        const stack = [];
+
+        for (let i = 0; i < cars.length; i++) {
+            const time = (target - cars[i][0]) / cars[i][1];
+
+            if (stack.length === 0 || stack[stack.length - 1] < time) {
+                stack.push(time);
+            }
+        }
+
+        return stack.length;
+    }
+}
