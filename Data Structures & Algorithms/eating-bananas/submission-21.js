466e98be2f974ab566e0f0033f07f00ede92f9ba
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-07-21 20:54:45 +0300

    Add: eating-bananas - submission-21

---
+++ Data Structures & Algorithms/eating-bananas/submission-21.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    // [1,4,3,2], h = 9
+    // speed = 1;
+    // []
+    minEatingSpeed(piles, h) {
+        let l = 1;
+        let r = Math.max(...piles);
+        let minSpeed = r;
+
+        while (l <= r) {
+            let total = 0;
+            const m = Math.floor((l + r) / 2);
+
+            for (let pile of piles) {
+                total += Math.ceil(pile / m);
+            }
+
+            if (total <= h) {
+                minSpeed = Math.min(minSpeed, m);
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return minSpeed
+    }
+}
