263a5556c9d59f646566c436ba3a4bd7d222520e
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-07-21 23:57:30 +0300

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canJump(nums) {
+        let reach = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (i > reach) return false;
+
+            reach = Math.max(reach, nums[i] + i);
+        }
+
+        return true;
+    }
+}
