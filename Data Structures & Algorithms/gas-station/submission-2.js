ddd98942a746610a6291dbae892a6ea96a42d307
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-07-22 21:38:29 +0300

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} gas
+     * @param {number[]} cost
+     * @return {number}
+     */
+    canCompleteCircuit(gas, cost) {
+        if (
+            gas.reduce((acc, val) => acc + val, 0) <
+            cost.reduce((acc, val) => acc + val, 0)
+        ) {
+            return -1;
+        }
+
+        let start = 0;
+        let total = 0;
+
+        for (let i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+
+            if (total < 0) {
+                total = 0;
+                start = i + 1;
+            }
+        }
+
+
+        return start;
+    }
+}
