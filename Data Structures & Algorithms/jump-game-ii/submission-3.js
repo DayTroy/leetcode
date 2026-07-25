66bc1a8dd1d72ec70c5c40b9ca70ea7dd8fc2111
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-07-25 12:59:23 +0300

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    jump(nums) {
+        let l = 0;
+        let r = 0;
+        let res = 0;
+
+        while (r < nums.length - 1) {
+            let farthest = 0;
+            for (let i = l; i <= r; i++) {
+                farthest = Math.max(farthest, nums[i] + i);
+            }
+            res++;
+            l = r + 1;
+            r = farthest;
+        }
+
+        return res;
+    }
+}
