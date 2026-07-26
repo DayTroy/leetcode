71c8fcb76630b137a5fe006e3d7d31b3413fbb4f
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-07-26 16:01:24 +0300

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @param {number} n
+     * @return {number}
+     */
+    myPow(x, n) {
+        let res = 1;
+        for (let i = 1; i <= Math.abs(n); i++) {
+            res *= x;
+        }
+        return n > 0 ? res : 1 / res;
+    }
+}
