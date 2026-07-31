bf647855c4d2e880c14b554efc031127667b79a5
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-07-31 21:45:35 +0300

    Add: rotate-matrix - submission-2

---
+++ Data Structures & Algorithms/rotate-matrix/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {void}
+     */
+    rotate(matrix) {
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = i; j < matrix.length; j++) {
+                const temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (let row of matrix) {
+            row.reverse();
+        }
+
+        return matrix;
+    }
+}
