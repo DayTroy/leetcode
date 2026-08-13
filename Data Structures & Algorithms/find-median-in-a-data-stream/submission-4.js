b25dd811224244d9594ae1e9dd55aa9aa0abf2ee
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-08-13 22:51:29 +0300

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.js
@@ -0,0 +1,38 @@
+class MedianFinder {
+    constructor() {
+        this.maxHeap = new MaxPriorityQueue(); 
+        this.minHeap = new MinPriorityQueue(); 
+    }
+
+    /**
+     *
+     * @param {number} num
+     * @return {void}
+     */
+    // max = [1, 2]
+    // min = [3, 4]
+    addNum(num) {
+        this.maxHeap.enqueue(num);
+
+        this.minHeap.enqueue(this.maxHeap.dequeue());
+
+        if (this.maxHeap.size() < this.minHeap.size()) {
+            this.maxHeap.enqueue(this.minHeap.dequeue());
+        }
+        
+    }
+
+    /**
+     * @return {number}
+     */
+    findMedian() {
+        if (this.maxHeap.size() > this.minHeap.size()) {
+            return this.maxHeap.front();
+        }
+
+        const a = this.maxHeap.front();
+        const b = this.minHeap.front();
+
+        return (a + b) / 2;
+    }
+}
