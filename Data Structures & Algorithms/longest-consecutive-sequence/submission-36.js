5349398ccba3247b8af76534617526c50cf2389f
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-08-14 22:48:07 +0300

    Add: longest-consecutive-sequence - submission-36

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-36.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const numSet = new Set(nums);
+        let longest = 0;
+
+        for (const num of numSet) {
+            if (!numSet.has(num - 1)) {
+                let len = 1;
+                while (numSet.has(num + len)) {
+                    len++;
+                }
+                longest = Math.max(longest, len);
+            }
+        }
+
+        return longest;
+    }
+}
