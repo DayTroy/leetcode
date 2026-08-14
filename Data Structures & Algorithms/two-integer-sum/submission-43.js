249f898eb143ab8ba841032687b5bef87447b32e
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-08-14 20:48:05 +0300

    Add: two-integer-sum - submission-43

---
+++ Data Structures & Algorithms/two-integer-sum/submission-43.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const data = new Map();
+
+        for (let i = 0; i < nums.length; i++) {
+            const diff = target - nums[i];
+            if (data.has(diff)) {
+                return [data.get(diff), i];
+            } else {
+                data.set(nums[i], i)
+            }
+        }
+    }
+}
