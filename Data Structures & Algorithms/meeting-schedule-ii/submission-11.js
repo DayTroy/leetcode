a604778a2e0793ae3e5959a8c19d50d9c8558599
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-08-22 18:37:58 +0300

    Add: meeting-schedule-ii - submission-11

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-11.js
@@ -0,0 +1,36 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals) {
+        let start = intervals.map(interval => interval.start).sort((a, b) => a - b);
+        let end = intervals.map(interval => interval.end).sort((a, b) => a - b);
+
+        let l = 0, r = 0;
+        let res = 0;
+        let count = 0;
+        while (l < intervals.length) {
+            if (start[l] < end[r]) {
+                l++;
+                count++;
+            } else {
+                r++;
+                count--;
+            }
+            res = Math.max(res, count);
+        }
+
+        return res;
+    }
+}
