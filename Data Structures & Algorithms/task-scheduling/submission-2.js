82d555ed2b14ff6c600f7264fdd07e7e9f3bec07
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-09-22 19:49:53 +0300

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {character[]} tasks
+     * @param {number} n
+     * @return {number}
+     */
+    leastInterval(tasks, n) {
+        let time = 0;
+        const frequency = new Map();
+
+        for (const task of tasks) {
+            frequency.set(task, (frequency.get(task) || 0) + 1);
+        }
+        const heap = new MaxPriorityQueue();
+        for (const [key, value] of frequency) {
+            heap.enqueue(value);
+        }
+
+        const cooldowns = [];
+
+        while (heap.size() > 0 || cooldowns.length > 0) {
+            time++;
+            if (heap.size() > 0) {
+                let value = heap.dequeue();
+                value--;
+                if (value > 0) {
+                    cooldowns.push([value, time + n]);
+                }
+            }
+
+            if (cooldowns.length > 0 && cooldowns[0][1] === time) {
+                heap.enqueue(cooldowns.shift()[0]);
+            }
+        }
+
+        return time;
+    }
+}
